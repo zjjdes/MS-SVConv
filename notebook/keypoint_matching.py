55e4477b9d77476c7e84f53a9e9a8905aa6424c5
Desmond Zhang <zjjsmail@gmail.com>
2021-07-12 19:03:57 +1000

    update

--- notebook/keypoint_matching.py
+++ notebook/keypoint_matching.py
@@ -1,5 +1,5 @@
 import os
-from os import listdir
+from os import listdir, remove
 from os.path import isfile, join
 import pathlib
 import time
@@ -18,8 +18,106 @@ from scipy.spatial.distance import cdist
 
 torch.cuda.set_per_process_memory_fraction(0.8)
 
+def remove_rover(pcd):
+    """Remove rover vehicle from the point cloud
 
-def read_pcd(path):
+    Args:
+        pcd ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    coords = np.asarray(pcd.points)
+
+    rover_width = 2 # x, 2
+    rover_length = 5 # y, 5
+    rover_height = 1.8 # z, 3.6
+
+    # Assuming sensor is the origin and is on top of the vehicle
+    coords = np.delete(coords, np.where((coords[:,0] > -rover_width / 2) & (coords[:,0] < rover_width / 2) & (coords[:,1] > -rover_length / 2) & (coords[:,1] < rover_length / 2) & (coords[:,2] < rover_height)), axis=0)
+
+    new_pc = o3d.geometry.PointCloud()
+    new_pc.points = o3d.utility.Vector3dVector(coords)
+
+    return new_pc
+
+def remove_ground(pcd):
+    """Remove ground points from the point cloud. Optimised for Velodyne HDL-32E
+
+    Args:
+        pcd ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    n_scan = 32
+    horizon_scan = 1800
+    ang_top = 30.0
+    ang_bottom = -10.0
+    ang_res_x = 360.0 / horizon_scan
+    ang_res_y = (ang_top-ang_bottom) / (n_scan - 1)
+    ground_scan_ind = 20
+    sensor_mount_angle = 0.0
+    sensor_minimum_range = 1.0
+
+    coords = np.asarray(pcd.points)
+
+    # Reorder points
+    total_pts = horizon_scan * ground_scan_ind
+    reordered_coords = np.zeros(shape=[total_pts, 4])
+    for [x, y, z] in coords:
+        if np.linalg.norm([x, y, z]) < sensor_minimum_range:
+            continue
+
+        vertical_angle = np.arctan2(z, np.linalg.norm([x, y])) * 180.0 / np.pi
+
+        if vertical_angle < ang_bottom or vertical_angle > ang_top:
+            continue
+
+        vert_id = np.round((vertical_angle - ang_bottom) / ang_res_y)
+
+        horizon_angle = np.arctan2(y, x) * 180.0 / np.pi
+
+        if horizon_angle < 0:
+            horizon_angle += 360
+
+        horizon_id = np.round(horizon_angle / ang_res_x)
+
+        index = int(horizon_id + vert_id * horizon_scan)
+
+        if index > total_pts:
+            continue
+
+        reordered_coords[index, :3] = [x, y, z]
+
+    # Ground removal
+    for i in range(reordered_coords.shape[0]):
+        lower_id = i
+        upper_id = i + horizon_scan
+
+        if upper_id >= reordered_coords.shape[0]:
+            continue
+
+        diffX = reordered_coords[upper_id, 0] - reordered_coords[lower_id, 0]
+        diffY = reordered_coords[upper_id, 1] - reordered_coords[lower_id, 1]
+        diffZ = reordered_coords[upper_id, 2] - reordered_coords[lower_id, 2]
+
+        angle = np.arctan2(diffZ, np.linalg.norm([diffX, diffY])) * 180.0 / np.pi
+
+        if np.abs(angle - sensor_mount_angle) <= 10:
+            reordered_coords[lower_id, 3] = 1
+            reordered_coords[upper_id, 3] = 1
+
+    reordered_coords = reordered_coords[np.logical_not(np.logical_and(reordered_coords[:, 0] == 0, reordered_coords[:, 1] == 0, reordered_coords[:, 2] == 0))]
+    
+    new_points = reordered_coords[:, :3][reordered_coords[:, -1] != 1]
+
+    new_pc = o3d.geometry.PointCloud()
+    new_pc.points = o3d.utility.Vector3dVector(new_points)
+
+    return new_pc
+
+def read_pcd(path, downsample=0, non_rover=True, non_ground=False):
     """Read point cloud
 
     Args:
@@ -29,12 +127,24 @@ def read_pcd(path):
         [type]: [description]
     """
     pcd = o3d.io.read_point_cloud(path)
-    data = Pair(pos=torch.from_numpy(np.asarray(pcd.points)).float(),
-                batch=torch.zeros(len(pcd.points)).long())
+
+    if downsample > 0:
+        pcd = pcd.voxel_down_sample(voxel_size)
+    
+    if non_rover:
+        pcd = remove_rover(pcd)
+
+    if non_ground:
+        pcd = remove_ground(pcd)
+
+    points = pcd.points
+
+    data = Pair(pos=torch.from_numpy(np.asarray(points)).float(),
+                batch=torch.zeros(len(points)).long())
     return data
 
 
-def ransac(pos_s, pos_t, feat_s, feat_t, distance_threshold=0.3):
+def ransac(pos_s, pos_t, feat_s, feat_t, distance_threshold=0.1):
     """Register two point clouds using RANSAC
 
     Args:
@@ -42,7 +152,7 @@ def ransac(pos_s, pos_t, feat_s, feat_t, distance_threshold=0.3):
         pos_t ([type]): [description]
         feat_s ([type]): [description]
         feat_t ([type]): [description]
-        distance_threshold (float, optional): [description]. Defaults to 0.3.
+        distance_threshold (float, optional): [description]. Defaults to 0.1.
 
     Returns:
         [type]: [description]
@@ -70,10 +180,40 @@ def ransac(pos_s, pos_t, feat_s, feat_t, distance_threshold=0.3):
     return result
 
 
+def fast_registration(pos_s, pos_t, feat_s, feat_t, distance_threshold=0.1):
+    """Register two point clouds based on Fast Global Registration
+        Q.-Y. Zhou, J. Park, and V. Koltun, Fast Global Registration, ECCV, 2016.
+
+    Args:
+        pos_s ([type]): [description]
+        pos_t ([type]): [description]
+        feat_s ([type]): [description]
+        feat_t ([type]): [description]
+        distance_threshold (float, optional): [description]. Defaults to 0.04.
+
+    Returns:
+        [type]: [description]
+    """
+    pcd_s = o3d.geometry.PointCloud()
+    pcd_s.points = o3d.utility.Vector3dVector(pos_s.numpy())
+    pcd_t = o3d.geometry.PointCloud()
+    pcd_t.points = o3d.utility.Vector3dVector(pos_t.numpy())
+
+    f_s = o3d.pipelines.registration.Feature()
+    f_s.data = feat_s.T.numpy()
+    f_t = o3d.pipelines.registration.Feature()
+    f_t.data = feat_t.T.numpy()
+    result = o3d.pipelines.registration.registration_fast_based_on_feature_matching(
+        source=pcd_s, target=pcd_t, source_feature=f_s, target_feature=f_t, 
+        option=o3d.pipelines.registration.FastGlobalRegistrationOption(
+            maximum_correspondence_distance=distance_threshold))
+    return result
+
+
 def load_lists(pos_file, map_file, ref_file, rov_file):
     # Crude/estimated positions of the vehicle
-    pos_xyz = np.genfromtxt(pos_file, delimiter=',').reshape((-1, 4))
-    pos_xyz = pos_xyz[:,1:]
+    pos_xyz = np.genfromtxt(pos_file, delimiter=',').reshape((-1, 3))
+    # pos_xyz = pos_xyz[:,1:]
 
     # Vehicle positions of the reference scans
     map_xyz = np.genfromtxt(map_file, delimiter=',').reshape((-1, 3))
@@ -87,7 +227,7 @@ def load_lists(pos_file, map_file, ref_file, rov_file):
     return pos_xyz, map_xyz, ref_id, rov_id
 
 
-def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, rov_file, max_ref, voxel_size, num_kpts):
+def main(velo_dir, out_dir, model_file, pos_file, map_file, ref_file, rov_file, max_ref, max_trials, min_corres, voxel_size, num_kpts, non_rover, non_ground, downsample):
     # Load reference list, rover list, reference vehicle positions, rover vehicle positions
     pos_xyz, map_xyz, ref_id, rov_id = load_lists(pos_file, map_file, ref_file, rov_file)
 
@@ -102,7 +242,8 @@ def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, ro
     model = PretainedRegistry.from_file(model_file, mock_property={})
 
     # Data augmentation settings
-    transfo = Compose([Random3AxisRotation(rot_x=180, rot_y=180, rot_z=180), GridSampling3D(size=voxel_size, quantize_coords=True, mode='last'), AddOnes(), AddFeatByKey(add_to_x=True, feat_name="ones")])
+    # transform = Compose([Random3AxisRotation(rot_x=180, rot_y=180, rot_z=180), GridSampling3D(size=voxel_size, quantize_coords=True, mode='last'), AddOnes(), AddFeatByKey(add_to_x=True, feat_name="ones")])
+    transform = Compose([GridSampling3D(size=voxel_size, quantize_coords=True, mode='last'), AddOnes(), AddFeatByKey(add_to_x=True, feat_name="ones")])
 
     for i in range(dists.shape[0]):
         t_init = time.time()
@@ -113,7 +254,7 @@ def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, ro
         # Locate the rover scan file
         rov_filename = str(rov_id[i])[2:-2]
         rov_path = join(velo_dir, rov_filename + '.ply')
-        data_t = transfo(read_pcd(rov_path)) # read rover scan
+        data_s = transform(read_pcd(rov_path, downsample, non_rover, non_ground)) # read rover scan
 
         # Indices of sorted distances to all reference cans for current rover scan
         sorted_idx = np.argsort(dists[i, :])
@@ -128,9 +269,9 @@ def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, ro
             ref_filename = str(ref_id[sorted_idx[j]])[2:-2]
             ref_path = join(velo_dir, ref_filename + '.ply')
 
-            data_s = transfo(read_pcd(ref_path)) # read reference scan
+            data_t = transform(read_pcd(ref_path, downsample, non_rover, non_ground)) # read reference scan
 
-            print('Processing ROVER: ' + rov_filename + ' with REFERENCE: ' + ref_filename)
+            print('Processing ROVER: ' + rov_filename + ' with REFERENCE: ' + ref_filename + ', d=' + str(dists[i, sorted_idx[j]]))
 
             # Compute the matches
             with torch.no_grad():
@@ -141,13 +282,23 @@ def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, ro
 
             rand_s = torch.randint(0, len(feat_s), (num_kpts, ))
             rand_t = torch.randint(0, len(feat_t), (num_kpts, ))
-            matches = get_matches(feat_s[rand_s], feat_t[rand_t], sym=True)
+
+            # matches = get_matches(feat_s[rand_s], feat_t[rand_t], sym=True)
+            # sel_s = matches[:,0]
+            # sel_t = matches[:,1]
 
             # Register scans using RANSAC
-            ransac_result = ransac(data_s.pos[rand_s], data_t.pos[rand_t], feat_s[rand_s], feat_t[rand_t], voxel_size)
-            # ransac_result = fast_global_registration(data_s.pos[rand_s], data_t.pos[rand_t])
-            transformation = ransac_result.transformation
-            inlier_rmse = ransac_result.inlier_rmse
+            for k in range(max_trials):
+                # reg_result = fast_registration(data_s.pos[rand_s], data_t.pos[rand_t], feat_s[rand_s], feat_t[rand_t], 0.1)
+                reg_result = ransac(data_s.pos[rand_s], data_t.pos[rand_t], feat_s[rand_s], feat_t[rand_t], 0.1)
+                # reg_result = ransac(data_s.pos[sel_s], data_t.pos[sel_t], feat_s[sel_s], feat_t[sel_t], 0.1)
+
+                if np.asarray(reg_result.correspondence_set).shape[0] >= min_corres:
+                    Tr = reg_result.transformation
+                    inlier_rmse = reg_result.inlier_rmse
+                    break
+                elif k == max_trials - 1:
+                    inlier_rmse = 0.0
 
             if inlier_rmse == 0.0 and j >= max_ref:
                 unprocessed = np.vstack((unprocessed, np.array([rov_filename])))
@@ -155,24 +306,26 @@ def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, ro
                 print(' - Failed to match ' + rov_filename + ' with any reference scan, skip.')
             elif inlier_rmse == 0.0:
                 j += 1
-                print(' - RMSE=' + str(inlier_rmse) + ', ' + rov_filename + 'to be reprocessed with the next nearest reference scan.')
+                print(' - ' + str(np.asarray(reg_result.correspondence_set).shape[0]) + ' correspondences, ' + rov_filename + ' to be reprocessed with the next nearest reference scan.')
             else:
-                corres = np.asarray(ransac_result.correspondence_set)
+                corres = np.asarray(reg_result.correspondence_set)
                 kpts_s = np.asarray(data_s.pos[rand_s][corres[:, 0], :]) # TODO: apply georeferencing here?
                 kpts_t = np.asarray(data_t.pos[rand_t][corres[:, 1], :])
+                # kpts_s = np.asarray(data_s.pos[sel_s][corres[:, 0], :]) # TODO: apply georeferencing here?
+                # kpts_t = np.asarray(data_t.pos[sel_t][corres[:, 1], :])
 
-                print(' - Registration successful! RMSE=' + str(inlier_rmse) + ', matched ' + str(corres.shape[0]) + ' keypoints.')
+                print(' - Registration successful after ' + str(k + 1) + ' trials with reference scan #' + str(j + 1) + '. RMSE=' + str(inlier_rmse) + ', matched ' + str(corres.shape[0]) + ' keypoints.')
 
-                # Collect results: [rover scan ID, reference scan ID, RMSE]
-                results = [[rov_filename], [ref_filename], [inlier_rmse]]
+                # Collect results: [reference scan ID, rover scan ID, RMSE]
+                results = [[ref_filename], [rov_filename], [inlier_rmse]]
 
                 # Save results
                 with open(join(result_dir, 'Ref_Rov_RMSE.csv'), 'w') as f:
                     writer = csv.writer(f)
                     writer.writerows(results)
-                    writer.writerows(transformation)
-                np.savetxt(join(result_dir, ref_filename + '_keypts.csv'), kpts_s, fmt='%1.4f', delimiter=',')
-                np.savetxt(join(result_dir, rov_filename + '_keypts.csv'), kpts_t, fmt='%1.4f', delimiter=',')
+                    writer.writerows(Tr)
+                np.savetxt(join(result_dir, rov_filename + '_keypts.csv'), kpts_s, fmt='%.5f', delimiter=',')
+                np.savetxt(join(result_dir, ref_filename + '_keypts.csv'), kpts_t, fmt='%.5f', delimiter=',')
 
                 processed = np.vstack((processed, np.array([rov_filename, ref_filename])))
 
@@ -188,18 +341,25 @@ def main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, ro
 if __name__ == '__main__':
     base_dir = '../../../UrbanNav/HK-Data20200314'
     velo_dir = join(base_dir, 'velodyne')
-    tr_dir = join(base_dir, 'Tr_loam_georeferencing')
-    out_dir = join(base_dir, 'keypoint_matching_output')
-    model_file = './models/MS_SVCONV_B4cm_X2_3head.pt'
-    pos_file = join(base_dir, 'ublox_sd_test.csv')
-    map_file = join(base_dir, 'velodyne_map/hdMap_xyz.csv')
-    ref_file = join(base_dir, 'map_list.txt')
-    rov_file = join(base_dir, 'test_list.txt')
-    max_ref = 5
-    voxel_size = 0.3
-    num_kpts = 2000
+    out_dir = join(base_dir, 'keypoint_matching_output_ETH_3000_0.04_5_NR_test3')
+    model_file = './models/MS_SVCONV_4cm_X2_3head_eth.pt' # ETH
+    # model_file = './models/MS_SVCONV_B4cm_X2_3head.pt' # ETH
+    # model_file = './models/MS_SVCONV_2cm_X2_3head_3dm.pt' # 3DMatch
+    # model_file = './models/MS_SVCONV_B2cm_X2_3head.pt' # TUM
+    pos_file = join(base_dir, 'ublox_sd_test3.csv')
+    map_file = join(base_dir, 'velodyne_map2/hdMap_xyz.csv')
+    ref_file = join(base_dir, 'map_list2.txt')
+    rov_file = join(base_dir, 'test_list3.txt')
+    max_ref = 3 # maximum number of reference scans to test
+    max_trials = 5 # maximum number of trials for RANSAC
+    min_corres = 5 # minimum number of correspondence to accept transformation
+    voxel_size = 0.04
+    num_kpts = 3000
+    non_rover = True # remove rover vehicle before processing
+    non_ground = False # remove ground points before processing
+    downsample = 0 # voxel size for downsampling, 0 means no downsampling
 
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    main(velo_dir, tr_dir, out_dir, model_file, pos_file, map_file, ref_file, rov_file, max_ref, voxel_size, num_kpts)
+    main(velo_dir, out_dir, model_file, pos_file, map_file, ref_file, rov_file, max_ref, max_trials, min_corres, voxel_size, num_kpts, non_rover, non_ground, downsample)
